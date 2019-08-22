fa7c5471d974ba7a78f8231ca396b3bf42830f64
py on <takuya.ya.e.ii.0mj@gmail.com>
2019-08-22 09:11:14 +0900

    BaseView/Presenter追加

---
+++ app/src/main/java/com/example/todo/BasePresenter.java
@@ -0,0 +1,5 @@
+package com.example.todo;
+
+public interface BasePresenter {
+    void start();
+}
---
+++ app/src/main/java/com/example/todo/BaseView.java
@@ -0,0 +1,5 @@
+package com.example.todo;
+
+public interface BaseView<T> {
+    void setPresenter(T presenter);
+}
